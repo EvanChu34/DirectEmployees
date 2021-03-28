dc653dd3ffbaeba09eb3208cfdfd574e3c0f41b3
Evan Chu <echu34@gmail.com>
2021-03-28 15:34:05 -0400

    adding some more

--- directemployees/src/employeepages/employee.js
+++ directemployees/src/employeepages/employee.js
@@ -3,11 +3,17 @@ import axios from "axios";
 import EmployeeData from "./EmployeeData";
 
 class Employee extends React.Component{
+    state = {
+        employeeRecords: [],
+    }
+    componentDidMount = () => {
+        axios
+        .get("https://randomuser.me/api/?results=500&nat=us")
+        .then((records) => {
+            
+
+        })
+    }
 
 
-
-
-
-
-    
 }
