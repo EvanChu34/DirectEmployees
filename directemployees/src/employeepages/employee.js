b358477db98f33d466dc425b13e1e2481902cbf4
Evan Chu <echu34@gmail.com>
2021-03-28 16:18:05 -0400

    getting the api in the code

--- directemployees/src/employeepages/employee.js
+++ directemployees/src/employeepages/employee.js
@@ -10,8 +10,20 @@ class Employee extends React.Component{
         axios
         .get("https://randomuser.me/api/?results=500&nat=us")
         .then((records) => {
-            
+            let employdata = records.data.results;
+            let employeeRecordstemp = [];
 
+            for (let i = 0; i < employdata.length; i++){
+                let record = {
+                    name: employdata[i].name,
+                    email: employdata[i].email,
+                    image: employdata[i].picture,
+                    phone: employdata[i].phone,
+                    dob: employdata[i].dob.date,
+                }
+                employeeRecordstemp.push(record)
+            }
+            this.setState({employeeRecords: employeeRecordstemp})
         })
     }
 
