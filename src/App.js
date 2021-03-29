fc26b4b23dbb904f28e6da050f1bc34d1e4937e0
Evan Chu <echu34@gmail.com>
2021-03-29 19:49:22 -0400

    moving the app out of the folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Employee from "./employeepages/employee";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="jumbotron">
+          DirectEmployees
+      </header>
+      <Employee />
+    </div>
+  );
+}
+
+export default App;
